d295cc17350f867a0a970d48bb9a87c5682c8ece
r4pt0s <r4pt0s@protonmail.com>
2021-09-21 14:04:24 +0000

    Deploying to gh-pages from @ zero-to-mastery/WebBlocks@58b687a550802169e157a9585b09efa490249d3c 🚀

---
+++ build/webblocks.esm.js
@@ -0,0 +1,1 @@
+import{p as t,b as m}from"./p-256962c8.js";(()=>{const m=import.meta.url,o={};return""!==m&&(o.resourcesUrl=new URL(".",m).href),t(o)})().then((t=>m([["p-50c4428f",[[1,"my-component",{first:[1],middle:[1],last:[1]}]]]],t)));
