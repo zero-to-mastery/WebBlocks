c909f2316bf852822c16f85ea30eceeb02a08a8f
Wolfgang Kreminger <r4pt0s@protonmail.com>
2021-09-21 15:39:58 +0200

    add sass plugin

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,10 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'webblocks',
+  plugins: [sass()],
+  globalStyle: 'src/global/base.scss',
   outputTargets: [
     {
       type: 'dist',
@@ -15,6 +18,11 @@ export const config: Config = {
     },
     {
       type: 'www',
+      /*
+          This line is important in order to see changes in the index.html file immediately via hot reloading if changes happen in stylesheets
+          without the need to re-build the entire thing
+      */
+      copy: [{ src: '../dist/webblocks/webblocks.css', dest: 'build/webblocks.css' }],
       serviceWorker: null, // disable service workers
     },
   ],
