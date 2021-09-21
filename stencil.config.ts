58b687a550802169e157a9585b09efa490249d3c
Wolfgang Kreminger <r4pt0s@protonmail.com>
2021-09-21 16:03:35 +0200

    final fix of stencil config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,5 +1,11 @@
-import { Config } from '@stencil/core';
+import { Config, Env } from '@stencil/core';
 import { sass } from '@stencil/sass';
+
+const dev: boolean = process.argv && process.argv.indexOf('--dev') > -1;
+const apiEnv: string = dev ? 'dev' : 'prod';
+const copyObj = { copy: [{ src: './../dist/webblocks/webblocks.css', dest: 'build/webblocks.css' }] };
+
+const shouldCopy = apiEnv === 'prod' ? {} : { ...copyObj };
 
 export const config: Config = {
   namespace: 'webblocks',
@@ -22,7 +28,7 @@ export const config: Config = {
           This line is important in order to see changes in the index.html file immediately via hot reloading if changes happen in stylesheets
           without the need to re-build the entire thing
       */
-      copy: [{ src: '/dist/webblocks/webblocks.css', dest: 'build/webblocks.css' }],
+      ...shouldCopy,
       serviceWorker: null, // disable service workers
     },
   ],
