0ac1c4659afc8c169948fa1893d4cc474b93e263
Wolfgang Kreminger <r4pt0s@protonmail.com>
2021-09-22 16:20:15 +0200

    add back color prop for test component

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -2,31 +2,36 @@ import { Component, Prop, h } from '@stencil/core';
 import { format } from '../../utils/utils';
 
 @Component({
-    tag: 'my-component',
-    styleUrl: 'my-component.css',
-    shadow: true,
+  tag: 'my-component',
+  styleUrl: 'my-component.css',
+  shadow: true,
 })
 export class MyComponent {
-    /**
-     * The first name
-     */
-    @Prop() first: string;
+  /**
+   * The first name
+   */
+  @Prop() first: string;
 
-    /**
-     * The middle name
-     */
-    @Prop() middle: string;
+  /**
+   * The middle name
+   */
+  @Prop() middle: string;
 
-    /**
-     * The last name
-     */
-    @Prop() last: string;
+  /**
+   * The last name
+   */
+  @Prop() last: string;
 
-    private getText(): string {
-        return format(this.first, this.middle, this.last);
-    }
+  /**
+   * The text color
+   */
+  @Prop() color: string;
 
-    render() {
-        return <h1>Hello, World! I'm {this.getText()}</h1>;
-    }
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  render() {
+    return <h1>Hello, World! I'm {this.getText()}</h1>;
+  }
 }
