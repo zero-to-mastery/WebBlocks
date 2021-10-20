050d9484f73de1b89ac138ba0843900d1b486e5b
Haider ali <75686258+Haider-Ali-Dev@users.noreply.github.com>
2021-10-20 23:51:51 +0400

    Updates

--- src/components/sign-in-form/sign-in-form.tsx
+++ src/components/sign-in-form/sign-in-form.tsx
@@ -7,17 +7,17 @@ import { Component, Host, h, Prop } from '@stencil/core';
 })
 export class SignInForm {
   @Prop() className: string;
-  @Prop() maxLengthEmail: number;
-  @Prop() maxLengthPassword: number;
-  @Prop() minLengthEmail: number;
-  @Prop() minLengthPassword: number;
+  @Prop() maxLengthEmail: string;
+  @Prop() maxLengthPassword: string;
+  @Prop() minLengthEmail: string;
+  @Prop() minLengthPassword: string;
   render() {
     return (
       <Host>
         <div class={`${this.className}`}>
           <div class="main">
-            <input minLength={this.minLengthEmail} maxLength={this.maxLengthEmail} placeholder={'Enter Your Email'} type={'email'} />
-            <input minLength={this.minLengthPassword} maxLength={this.maxLengthPassword} placeholder={'Enter Your Password'} type={'password'} />
+            <input minLength={Number(this.minLengthEmail)} maxLength={Number(this.maxLengthEmail)} placeholder={'Enter Your Email'} type={'email'} />
+            <input minLength={Number(this.minLengthPassword)} maxLength={Number(this.maxLengthPassword)} placeholder={'Enter Your Password'} type={'password'} />
             <button class="btn">Sign In</button>
           </div>
         </div>
