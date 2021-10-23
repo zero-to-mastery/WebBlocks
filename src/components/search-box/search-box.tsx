a3ee764c1fe30383add91ac24539e7c2fc43d1fe
juancwu <jc@macbook.com>
2021-10-23 00:19:47 -0400

    mimic query api request for autocomplete

--- src/components/search-box/search-box.tsx
+++ src/components/search-box/search-box.tsx
@@ -1,10 +1,7 @@
 import { Component, Host, h, Prop, State, Watch } from '@stencil/core';
 
-// const mockData = {
-//   'apple': 2,
-//   'amazing spider-man': 4,
-//   'banana': 10,
-// };
+// this data is just a bunch of english words
+import mockData from './data.json';
 
 @Component({
   tag: 'search-box',
@@ -21,6 +18,7 @@ export class SearchBox {
   @Prop() placeholder: string = 'Enter your search';
 
   @State() query: string = '';
+  @State() searchResult: string[] = [];
 
   timer: any = null; // not sure what the return type is when setTimeout is called in the browser
 
@@ -32,13 +30,23 @@ export class SearchBox {
 
     this.timer = setTimeout(() => {
       if ((newQuery = this.query)) {
-        this.search();
+        this.search(this.query);
       }
     }, 500);
   }
 
-  search() {
+  search(query: string) {
     // todo: make get request for real data
+    let regex = new RegExp(query);
+
+    let matches: string[] = [];
+    for (let term in mockData) {
+      if (regex.test(term)) {
+        matches.push(term);
+      }
+    }
+
+    this.searchResult = matches;
   }
 
   updateQueryState(e: Event) {
@@ -72,6 +80,16 @@ export class SearchBox {
               />
             </div>
           </div>
+          {this.searchResult.length > 0 && <div class="line"></div>}
+          {this.searchResult.length > 0 ? (
+            <div class="suggestions">
+              <ul>
+                {this.searchResult.map(r => (
+                  <li>{r}</li>
+                ))}
+              </ul>
+            </div>
+          ) : null}
         </form>
       </Host>
     );
